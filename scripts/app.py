a541c2a61b1339da9dc23f3a3fa0bc1e33dec324
LukeHalasy <lhalasy@gmail.com>
2018-10-20 11:57:06 -0400

    Got the api request to work, it succesfully sends back emojis

--- scripts/app.py
+++ scripts/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request 
 from getImageLabels import getImageLabels
+from emojiFinder import getEmojis
 
 app = Flask(__name__)
 
@@ -9,9 +10,16 @@ def test():
 
     image = None
     if 'image' in data:
+        # Get rid of the type-define text of the request
         image = data['image']
         image = image.split(",")[1]
-        return getImageLabels(image)
+
+        # Get the labels of the image with clarify
+        imageLabels = getImageLabels(image)
+
+        # Get related emojis to the labels
+        emojis = getEmojis(imageLabels)
+        return ', '.join(list(emojis))
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000) #run app in debug mode on port 5000
